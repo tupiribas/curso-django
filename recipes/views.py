545cbb7f5cfba051f2ac07c4d75f0c45ee7aca84
tupiribas <tupiguedes@outlook.com>
2024-07-27 00:45:19 -0300

    Header in Home

--- recipes/views.py
+++ recipes/views.py
@@ -3,12 +3,4 @@ from django.http import HttpResponse
 
 
 def home(request):
-    return render(request, 'recipies/home.html')
-
-
-def sobre(request):
-    return HttpResponse('Sobre')
-
-
-def contato(request):
-    return HttpResponse('Contato')
+    return render(request, 'recipes/home.html', context={'name': "Recipes"})
