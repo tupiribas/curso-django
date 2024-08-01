2b085d651e3b2586b2db0899d0b617f852a129c1
tupiribas <tupiguedes@outlook.com>
2024-07-31 22:26:16 -0300

    Implement Factory data fakes

--- recipes/views.py
+++ recipes/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-
+from utils.recipes.factory import make_recipe
 
 def home(request):
-    return render(request, 'recipes/pages/home.html', context={'name': "Home"})
+    return render(request, 'recipes/pages/home.html', context={
+        'recipes': [make_recipe() for _ in range(10)],
+    })
 
 def recipe(request, id):
-    return render(request, 'recipes/pages/recipe-view.html', context={'name': "Recipes"})
+    return render(request, 'recipes/pages/recipe-view.html', context={
+        'recipe': make_recipe(),
+        'is_detail_page': True,
+    })
