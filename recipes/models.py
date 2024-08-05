18d14bafc0adbc276819dbf05502ce46a7ebd857
tupiribas <tupiguedes@outlook.com>
2024-08-04 21:37:41 -0300

    config file static in urls

--- recipes/models.py
+++ recipes/models.py
@@ -15,8 +15,8 @@ class Recipe(models.Model):
     slug = models.SlugField()
     preparation_time = models.IntegerField()
     preparation_time_unit = models.CharField(max_length=65)
-    servings_time = models.IntegerField()
-    servings_time_unit = models.CharField(max_length=65)
+    servings = models.IntegerField()
+    servings_unit = models.CharField(max_length=65)
     preparation_steps = models.TextField()
     preparation_steps_is_html = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
@@ -28,3 +28,6 @@ class Recipe(models.Model):
     category = models.ForeignKey(
         Category, on_delete=models.SET_NULL, null=True)
     author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+
+    def __str__(self) -> str:
+        return self.title
