dc0e88e2ca4543516cfc02bea30fe186c2dbddd0
tupiribas <tupiguedes@outlook.com>
2024-08-11 02:35:13 -0300

    Name of category in title

--- recipes/views.py
+++ recipes/views.py
@@ -12,10 +12,11 @@ def home(request):
 
 
 def category(request, category_id):
-    recipes = Recipe.objects.filter(category__id=category_id, is_published=True).order_by('-id')
+    recipes = Recipe.objects.filter(
+        category__id=category_id, is_published=True).order_by('-id')
     return render(request, 'recipes/pages/home.html', context={
         'recipes': recipes,
-        'title_page': 'Category',
+        'title_page': f'{recipes.first().category.name} - Category',
     })
 
 
