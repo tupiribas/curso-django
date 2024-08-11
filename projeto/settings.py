219a213b4da6063b392590b77abe349b618d361a
tupiribas <tupiguedes@outlook.com>
2024-08-11 00:36:51 -0300

    Remove problem in category

--- projeto/settings.py
+++ projeto/settings.py
@@ -117,6 +117,7 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR / 'base_static', ]
 STATIC_ROOT = BASE_DIR / 'static'
 
+# Imagens
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
--- recipes/models.py
+++ recipes/models.py
@@ -11,7 +11,7 @@ class Category(models.Model):
 
 class Recipe(models.Model):
     title = models.CharField(max_length=65)
-    descriptions = models.CharField(max_length=165)
+    description = models.CharField(max_length=165)
     slug = models.SlugField()
     preparation_time = models.IntegerField()
     preparation_time_unit = models.CharField(max_length=65)
