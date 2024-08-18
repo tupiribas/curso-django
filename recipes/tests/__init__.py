1e936d3f81154fb3f6c7a529f61a84fbb685f6ee
tupiribas <tupiguedes@outlook.com>
2024-08-18 03:06:32 -0300

    separating tests

---
+++ recipes/tests/__init__.py
@@ -0,0 +1,6 @@
+from . import test_recipe_urls
+from . import test_recipe_views
+
+
+RecipeURLsTest = test_recipe_urls.RecipeURLsTest
+RecipeViewsTest = test_recipe_views.RecipeViewsTest
--- recipes/tests/test_recipe_views.py
+++ recipes/tests/test_recipe_views.py
@@ -4,20 +4,6 @@ from django.urls import resolve, reverse
 from recipes import views
 
 
-class RecipeURLsTest(TestCase):
-    def test_recipe_home_url_is_correct(self):
-        url = reverse('recipes:home')
-        self.assertEqual(url, '/')
-
-    def test_recipe_category_url_is_correct(self):
-        url = reverse('recipes:category', kwargs={'category_id': 1})
-        self.assertEqual(url, '/recipe/category/1/')
-
-    def test_recipe_details_url_is_correct(self):
-        url = reverse('recipes:recipe', kwargs={'id': 1})
-        self.assertEqual(url, '/recipe/1/')
-
-
 class RecipeViewsTest(TestCase):
     def test_recipe_home_views_function_is_correct(self):
         view_home = resolve(reverse('recipes:home'))
@@ -35,3 +21,5 @@ class RecipeViewsTest(TestCase):
         view_details = resolve(reverse('recipes:recipe', kwargs={'id': 1}))
         view_details_imported = views.recipe
         self.assertIs(view_details.func, view_details_imported)
+    
+    def test_recipe_home_view_re(self):
