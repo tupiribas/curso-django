c76bda03faa4e5144205604562aa0cc80f3fa9be
tupiribas <tupiguedes@outlook.com>
2024-08-18 02:44:14 -0300

    Using resolve to understand is beging used

--- recipes/tests.py
+++ recipes/tests.py
@@ -1,5 +1,7 @@
 from django.test import TestCase
-from django.urls import reverse
+from django.urls import resolve, reverse
+
+from recipes import views
 
 
 class RecipeURLsTest(TestCase):
@@ -14,3 +16,22 @@ class RecipeURLsTest(TestCase):
     def test_recipe_details_url_is_correct(self):
         url = reverse('recipes:recipe', kwargs={'id': 1})
         self.assertEqual(url, '/recipe/1/')
+
+
+class RecipeViewsTest(TestCase):
+    def test_recipe_home_views_function_is_correct(self):
+        view_home = resolve(reverse('recipes:home'))
+        view_home_imported = views.home
+        self.assertIs(view_home.func, view_home_imported)
+
+    def test_recipe_category_views_function_is_correct(self):
+        view_category = resolve(
+            reverse('recipes:category', kwargs={'category_id': 1})
+        )
+        view_category_imported = views.category
+        self.assertIs(view_category.func, view_category_imported)
+
+    def test_recipe_details_views_function_is_correct(self):
+        view_details = resolve(reverse('recipes:recipe', kwargs={'id': 1}))
+        view_details_imported = views.recipe
+        self.assertIs(view_details.func, view_details_imported)
