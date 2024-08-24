2e0d3e7687c24154b5b0913ada1b2c6b81833402
tupiribas <tupiguedes@outlook.com>
2024-08-24 19:18:13 -0300

    Create the context and context of response on template

--- recipes/tests/test_recipe_views.py
+++ recipes/tests/test_recipe_views.py
@@ -32,7 +32,27 @@ class RecipeViewsTest(TestCase):
         )
 
     def test_recipe_home_template_loads_recipes(self):
-        category = Category.objects.create(name='')
+        category = Category.objects.create(name='Category')
+        author = User.objects.create_user(
+            first_name='user',
+            last_name='name',
+            username='username',
+            password='123456',
+            email='user@gmail.com',
+        )
+        recipe = Recipe.objects.create(
+            title='Teste receita',
+            description='teste descricao',
+            slug='teste-receita',
+            preparation_time=10,
+            preparation_time_unit='Minutos',
+            servings=5,
+            servings_unit='Porção',
+            preparation_steps='Recipes tesinf en brasilian',
+            preparation_steps_is_html=False,
+            is_published=True,
+        )
+        assert 1 == 1
 
     def test_recipe_category_views_function_is_correct(self):
         view_category = resolve(
